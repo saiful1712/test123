b6272d1861d671c9d14bc53c9b4bf52f962e1ea7
saiful1712 <66183029+saiful1712@users.noreply.github.com>
2020-08-05 09:10:38 +0100

    Update Demo.java

--- src/main/java/javaApp/Demo.java
+++ src/main/java/javaApp/Demo.java
@@ -2,6 +2,8 @@ package javaApp;
 public class Demo {
 
     public static void main(String[] args) {
+        
+        System.out.println("Welcome to first maven war project");
       
         //Insert your code here
     }
